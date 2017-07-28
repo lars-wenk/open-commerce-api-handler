f6df962b97579ccb67976d0eb8559e3d614afe53
Lars Wenk <flying.lars@gmail.com>
2017-07-28 15:33:17 +0200

    add negative basicAuth test

--- test/basicAuth.test.js
+++ test/basicAuth.test.js
@@ -1,11 +1,33 @@
 const basicAuth = require('../ocapi/basicAuth');
-
-test('It should be return a object', () => {
-  expect(typeof basicAuth.getToken()).toBe('object');
-});
+const config = require('../config/ocapi.json');
 
 test('it sould be return a token', () => {
   return basicAuth.getToken().then(token => {
-    expect(token).toMatch(new RegExp('[a-z0-9\-]{36}'));
+    expect(token.access_token).toMatch(new RegExp('[a-z0-9\-]{36}'));
   });
+});
+
+test('it sould be return a 500 error, because of wrong Authorization Hash', () => {
+  let dynOptions = {
+    headers: {
+      Authorization: `Basic 12345`,
+      'Content-Type': 'application/x-www-form-urlencoded',
+    }
+  };
+  return basicAuth.getToken(dynOptions).catch(err => {
+    expect(err.message).toMatch('StatusCodeError: 500');
+  })
+});
+
+test('it sould be return a 400 error, because of wrong client_id', () => {
+  let dynOptions = {
+    uri:`https://${config.hostname}/dw/oauth2/access_token?client_id=12345`,
+    headers: {
+      Authorization: `Basic ${config.basicAuth}`,
+      'Content-Type': 'application/x-www-form-urlencoded',
+    }
+  };
+  return basicAuth.getToken(dynOptions).catch(err => {
+    expect(err.message).toMatch('StatusCodeError: 400');
+  })
 });
